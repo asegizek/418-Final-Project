fee757ea128f8f086e712fa51efec377fd36cf3e
Adilet Segizekov <asegizek@GHC31.GHC.ANDREW.CMU.EDU>
2016-11-28 22:45:47 -0500

    added platformgl.h

---
+++ code/platformgl.h
@@ -0,0 +1,12 @@
+#ifndef __PLATFORM_GL_H__
+#define __PLATFORM_GL_H__
+
+#ifdef __APPLE__
+#include <GLUT/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+#endif
